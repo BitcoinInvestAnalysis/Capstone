c545c8043f637b59f387f4c7c44115a8a708d94e
Duc-Hiep <duc-hiep.hoang@gmx.de>
2022-03-21 12:26:55 +0100

    Wrote a get stock daily function

--- script.py
+++ script.py
@@ -16,9 +16,9 @@ import time
 
 # Specifing figure layout
 # %matplotlib inline
-plt.style.use('fivethirtyeight')
-sns.set_color_codes('bright')
-plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
+plt.style.use("fivethirtyeight")
+sns.set_color_codes("bright")
+plot_kwds = {"alpha": 0.25, "s": 80, "linewidths": 0}
 plt.rcParams["figure.figsize"] = (15, 10)
 
 
@@ -61,82 +61,19 @@ def write_data(engine, dataframe, table_name):
             engine = None
 
 
-def get_weather_hourly(lat, lon, start, end):
-    url = "https://meteostat.p.rapidapi.com/point/hourly"
-    querystring = {"lat": lat, "lon": lon, "start": start, "end": end}
-    headers = {
-        "x-rapidapi-host": os.getenv("x-rapidapi-host"),
-        "x-rapidapi-key": os.getenv("x-rapidapi-key"),
+def get_stock_daily(symbol, outputsize, datatype):
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": {symbol},
+        "outputsize": {outputsize},
+        "datatype": {datatype},
     }
-
-    return requests.request("GET", url, headers=headers, params=querystring)
-
-
-def get_weather_daily(lat, lon, start, end):
-    url = "https://meteostat.p.rapidapi.com/point/daily"
-    querystring = {"lat": lat, "lon": lon, "start": start, "end": end}
     headers = {
         "x-rapidapi-host": os.getenv("x-rapidapi-host"),
         "x-rapidapi-key": os.getenv("x-rapidapi-key"),
     }
     return requests.request("GET", url, headers=headers, params=querystring)
-
-
-def normalize_and_set_airport_origin(response, airport_origin):
-    weather_df = pd.json_normalize(response.json(), record_path="data", errors="ignore")
-
-    weather_df["origin"] = airport_origin
-    return weather_df
-
-
-def rename_columns_hourly(df):
-    df.rename(
-        columns={
-            "dwpt": "dew_point",
-            "rhum": "rel_humidity_%",
-            "prcp": "precipitation_mm",
-            "snow": "snow_mm",
-            "wdir": "wind_dir_degrees",
-            "wspd": "wind_speed_km/h",
-            "wpgt": "peak_wind_km/h",
-            "pres": "air_pressure_hPa",
-            "tsun": "sun_hours_min",
-            "coco": "weather_condition_code",
-        },
-        inplace=True,
-    )
-    return df
-
-
-def rename_columns_daily(df):
-    df.rename(
-        columns={
-            "tavg": "avg_temperature",
-            "tmin": "min_temperature",
-            "tmax": "max_temperature",
-            "prcp": "precipitation_mm",
-            "snow": "max_snow_depth_mm",
-            "wdir": "avg_wind_dir_degrees",
-            "wspd": "avg_wind_speed_km/h",
-            "wpgt": "peak_wind_gust_km/h",
-            "pres": "air_pressure_hPa",
-            "tsun": "sun_hours_min",
-        },
-        inplace=True,
-    )
-    return df
-
-
-def prepared_weather_hourly(lat, lon, start, end, airport_origin):
-    resp = get_weather_hourly(lat, lon, start, end)
-    df = normalize_and_set_airport_origin(resp, airport_origin)
-    return rename_columns_hourly(df)
-
-
-def prepared_weather_daily(lat, lon, start, end, airport_origin):
-    resp = get_weather_daily(lat, lon, start, end)
-    df = normalize_and_set_airport_origin(resp, airport_origin)
-    return rename_columns_daily(df)
 
 
 # defining a utility function for testing the clustering algorithms
