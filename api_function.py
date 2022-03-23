2e60c2f2cfbee4655279510ad7f1f031bba91f20
Duc-Hiep <duc-hiep.hoang@gmx.de>
2022-03-23 11:50:15 +0100

    Used the new api function to get stock data

--- api_function.py
+++ api_function.py
@@ -2,8 +2,12 @@
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-import os
 
+import os
+import pandas_datareader.data as web
+import pandas_datareader as pdr
+from datetime import datetime
+import datetime as dt
 import requests
 
 
@@ -20,3 +24,13 @@ def get_stock_daily(symbol, outputsize, datatype):
         "x-rapidapi-key": os.getenv("x-rapidapi-key"),
     }
     return requests.request("GET", url, headers=headers, params=querystring)
+
+
+def get_df_stock_daily(symbol, start_date="2012-01-01", end_date="2021-12-31"):
+    return web.DataReader(
+        symbol,
+        "av-daily",
+        start=datetime.strptime(start_date, "%Y-%m-%d").date(),
+        end=datetime.strptime(end_date, "%Y-%m-%d").date(),
+        api_key=os.getenv("x-rapidapi-key"),
+    )
