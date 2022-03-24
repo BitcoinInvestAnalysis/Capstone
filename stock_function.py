9607349f4e5ba5ffc72946e2bf9dbfe8ef55869f
Duc-Hiep <duc-hiep.hoang@gmx.de>
2022-03-24 15:51:31 +0100

    Lets make some money!

--- stock_function.py
+++ stock_function.py
@@ -18,3 +18,41 @@ def calc_volatility(df, start_date, end_date, trading_day):
     else:
         df = add_log_returns_column(df, start_date, end_date)
         return df["log_returns"].std() * trading_day**0.5
+
+
+def revenue(df, start, end, invest):
+    df.columns = [col.replace(" ", "_").lower() for col in df.columns]
+    df.index = pd.to_datetime(df.index)
+    if (
+        invest
+        / (df.loc[df.index == start].open)[
+            df[df.index == start].index.values.astype(int)[0]
+        ]
+    ) <= (df.loc[df.index == start].volume)[
+        df[df.index == start].index.values.astype(int)[0]
+    ]:
+        profit = (df.loc[df.index == end].close)[
+            df[df.index == end].index.values.astype(int)[0]
+        ] - (df.loc[df.index == start].open)[
+            df[df.index == start].index.values.astype(int)[0]
+        ]
+        profit *= (
+            invest
+            / (df.loc[df.index == start].open)[
+                df[df.index == start].index.values.astype(int)[0]
+            ]
+        )
+        return profit
+    else:
+        print("That many Stocks are not available")
+
+
+def revenue_ratio(df, start, end):
+    df.columns = [col.replace(" ", "_").lower() for col in df.columns]
+
+    profit = (df.loc[df.index == end].close)[
+        df[df.index == end].index.values.astype(int)[0]
+    ] / (df.loc[df.index == start].open)[
+        df[df.index == start].index.values.astype(int)[0]
+    ]
+    return profit
